f214f1ccdc9419ec51ee19e2b8dc097f61f38c89
Atharva <147217989+atharvanev@users.noreply.github.com>
2026-04-30 17:59:23 -0700

    created parsing logic, but no dedepub protection

--- crawling/webcrawler/spiders/wiki_spider.py
+++ crawling/webcrawler/spiders/wiki_spider.py
@@ -19,19 +19,28 @@ class Spider_Wiki_Scraper(scrapy.Spider):
     async def start(self):
         seed_urls = load_seed_urls("seed_urls.txt")
         for url in seed_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, meta={"depth": 0})
 
     #Get HTML of pages and parse them
-    def parse(self, response) -> WebcrawlerItem:
-        
+    def parse(self, response):
+        depth = response.meta.get("depth", 0) #default fallback to 0 if depth is not set
+
         page_item = WebcrawlerItem()
 
         page_item["url"] = response.url
         page_item["title"] = response.xpath("//title/text()").get()
-        page_item["body"] = response.xpath("//body//text()").getall()
-        #page_item["creation_date"] = response.headers.get("Date", "").decode("utf-8")
+        page_item["body"] = response.xpath("//p//text()").getall()
         page_item["crawled_at"] = datetime.now().isoformat()
-        page_item["depth"] = depth+1
-        page_item["outgoing_links"] = response.xpath("//a/@href").getall()
+        page_item["depth"] = depth
+        raw_links = response.xpath("//a/@href").getall()
+        absolute_links = [response.urljoin(l) for l in raw_links if not l.startswith("#")]
+        page_item["outgoing_links"] = absolute_links
 
-        return page_item
+
+        #based on my knolwedge scrappy the Scrappy has its own Queue system and it will handle the scheduling of requests. So we just need to yield new requests and Scrapy will take care of the rest.
+        for link in absolute_links:
+            #add some logic for dedepublication 
+            # looking in settings.py to change the Depth limit to do full test
+            yield scrapy.Request(url=link, callback=self.parse, meta={"depth": depth + 1}) #increase depth for outgoing links and puts it in the Queue
+
+        yield page_item
