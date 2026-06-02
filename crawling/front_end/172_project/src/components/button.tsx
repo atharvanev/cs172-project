3929d30e5320965fb8506363e891a58bf08c39ff
Austin <austintle56@gmail.com>
2026-06-02 16:18:42 -0700

    Setting up simple UI

--- crawling/front_end/172_project/src/components/button.tsx
+++ crawling/front_end/172_project/src/components/button.tsx
@@ -1,9 +1,17 @@
+type ButtonProps = {
+  disabled?: boolean;
+};
 
+const Button = ({ disabled = false }: ButtonProps) => {
+  return (
+    <button
+      className="h-10 rounded-r-md border border-l-0 border-gray-900 bg-gray-900 px-4 text-sm font-medium text-white transition hover:bg-gray-700 disabled:cursor-not-allowed disabled:opacity-50"
+      disabled={disabled}
+      type="submit"
+    >
+      Search
+    </button>
+  );
+};
 
-const button = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default button;
+export default Button;
